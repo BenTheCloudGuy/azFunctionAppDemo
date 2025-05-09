01241fe8d0157d9e1d5eea9cc306076041dc90cd
BenTheBuilder <47876371+BenTheCloudGuy@users.noreply.github.com>
2025-05-09 16:03:12 +0000

    fix vars.tf issue

--- infra/vars.tf
+++ infra/vars.tf
@@ -23,39 +23,3 @@ variable "app_service_plan_name" {
   description = "The name of the App Service Plan."
   type        = string
 }
-
-variable "web_frontend_name" {
-  description = "The name of the Web Frontend (Python)."
-  type        = string
-}
-
-variable "function_app_name" {
-  description = "The name of the Function App (PowerShell)."
-  type        = string
-}
-
-variable "sql_server_name" {
-  description = "The name of the SQL Server."
-  type        = string
-}
-
-variable "sql_database_name" {
-  description = "The name of the SQL Database."
-  type        = string
-}
-
-variable "admin_username" {
-  description = "The admin username for the SQL Database."
-  type        = string
-}
-
-variable "admin_password" {
-  description = "The admin password for the SQL Database."
-  type        = string
-  sensitive   = true
-}
-
-variable "key_vault_name" {
-  description = "The name of the Key Vault."
-  type        = string
-}
